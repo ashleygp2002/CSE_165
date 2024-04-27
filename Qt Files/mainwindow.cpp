fa85794f3236f9958ddc1673b6c4b4b4bf78d6cc
gdiaz38 <gdiaz38@ucmerced.edu>
2024-04-26 17:58:09 -0700

    uplaoded code
    using this code we are able to get rid of error and save correctly

--- Qt Files/mainwindow.cpp
+++ Qt Files/mainwindow.cpp
@@ -145,6 +145,10 @@
 //     ui->listWidget->clear();
 // }
 
+
+
+
+/*
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <Qfile>
@@ -219,3 +223,90 @@ void MainWindow::on_btnRemoveAll_clicked()
 {
     ui->listWidget->clear();
 }
+*/
+
+
+
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QStandardPaths>
+#include <QMessageBox>
+#include <QRegularExpression>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("Todo List");
+
+    QFile file(path);
+
+    if(!file.open(QIODevice::ReadWrite)){
+        QMessageBox::information(0, "Error", file.errorString());
+    }
+
+    QTextStream in(&file);
+
+    while(!in.atEnd()) {
+        QString task = in.readLine();
+        if (!task.isEmpty()) {
+            QListWidgetItem* item = new QListWidgetItem("• " + task, ui->listWidget);
+            ui->listWidget->addItem(item);
+            item->setFlags(item->flags() | Qt::ItemIsEditable);
+        }
+    }
+
+    file.close();
+}
+
+MainWindow::~MainWindow()
+{
+    saveToDoList(); // Save ToDo List when the application is closed
+    delete ui;
+}
+
+void MainWindow::saveToDoList()
+{
+    QFile file(path);
+
+    if(!file.open(QIODevice::ReadWrite)){
+        QMessageBox::information(0, "Error", file.errorString());
+    }
+
+    QTextStream out(&file);
+
+    for (int i = 0; i < ui->listWidget->count(); ++i){
+        QString task = ui->listWidget->item(i)->text();
+        task.remove(QRegularExpression("^\\s*•\\s*"));
+        out << task << "\n";
+    }
+
+    file.close();
+}
+
+void MainWindow::on_btnAdd_clicked()
+{
+    QListWidgetItem* item = new QListWidgetItem("• " + ui->txtTask->text(), ui->listWidget);
+    ui->listWidget->addItem(item);
+    item->setFlags(item->flags() | Qt::ItemIsEditable);
+    ui->txtTask->clear();
+    ui->txtTask->setFocus();
+    saveToDoList(); // Save ToDo List when an item is added
+}
+
+void MainWindow::on_btnRemove_clicked()
+{
+    QListWidgetItem* item = ui->listWidget->takeItem(ui->listWidget->currentRow());
+    delete item;
+    saveToDoList(); // Save ToDo List when an item is removed
+}
+
+void MainWindow::on_btnRemoveAll_clicked()
+{
+    ui->listWidget->clear();
+    saveToDoList(); // Save ToDo List when all items are removed
+}
+
